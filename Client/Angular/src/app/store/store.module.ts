d8c0b4fad0cdb1ed0707f95add2803c54b4e1eb0
Nang Nguyen <nang.nguyen@evizi.com>
2024-11-03 11:53:46 +0700

    Add ocelet api gateway

--- Client/Angular/src/app/store/store.module.ts
+++ Client/Angular/src/app/store/store.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
 import { SharedModule } from "../shared/shared.module";
+import { ProductItemsComponent } from './product-items/product-items.component';
 
 @NgModule({
-  declarations: [StoreComponent],
+  declarations: [StoreComponent, ProductItemsComponent],
   imports: [CommonModule, SharedModule],
   exports: [StoreComponent],
 })
